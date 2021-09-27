ba7ee393e03118394cbc186392bbae49161874bf
Денис Гришкевич <cris12@mail.ru>
2021-09-27 21:32:38 +0300

    first commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,8 @@
+public class BonusMilesService {
+    public int calculate(int amount) {
+            int bonusGrade = 20;
+            int miles = amount / bonusGrade; {
+            }
+            return miles;
+        }
+}
